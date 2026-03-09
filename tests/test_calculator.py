21574941d81bbcb0eb46ee41ea754c5121d69b47
decembra <foreverundersky@gmail.com>
2026-03-09 17:16:17 +0300

    feat(calculator): add division function and its tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from src.calculator import add
+from src.calculator import add, divide
 
 
 def test_add():
@@ -11,3 +11,11 @@ def test_add_raises_type_error_on_string_input():
     """Тест ошибки TypeError при складывании числа и строки"""
     with pytest.raises(TypeError):
         add(5, "hello")
+
+
+def test_divide_by_zero_raises_value_error_with_message():
+    with pytest.raises(ValueError) as excinfo:
+        divide(10, 0)
+
+    # Проверяем, что текст ошибки содержит нужную фразу
+    assert "Нельзя делить на ноль" in str(excinfo.value)
